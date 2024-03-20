5359448ff7e162f40d58733b35a6189b5774456e
Keithjmm <joenormal79@gmail.com>
2024-03-20 20:16:53 +0000

    Specify Python buildpack

--- eventtimer_backend/eventtimer_backend/settings.py
+++ eventtimer_backend/eventtimer_backend/settings.py
@@ -13,8 +13,11 @@ import os
 import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
+import django_heroku
 
 load_dotenv()
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,6 +134,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
