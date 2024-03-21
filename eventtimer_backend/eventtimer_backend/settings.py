4c8755b21ac50006a5285e31bb7ff4f62dbd89db
Keithjmm <joenormal79@gmail.com>
2024-03-21 19:56:39 +0000

    update .env file

--- eventtimer_backend/eventtimer_backend/settings.py
+++ eventtimer_backend/eventtimer_backend/settings.py
@@ -18,8 +18,6 @@ import re
 
 load_dotenv()
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -49,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'dj_rest_auth.registration',
+    # 'dj_rest_auth.registration',
     'corsheaders',
     'api',
 ]
@@ -140,7 +138,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -151,3 +149,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
